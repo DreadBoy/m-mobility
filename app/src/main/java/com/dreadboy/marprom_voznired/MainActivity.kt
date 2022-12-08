5619daeb34c84c6c869d6d548a609fa71ce3a100
Matic Leva <vertical3life@gmail.com>
2022-12-08 21:32:18 +0100

    Implement favourite bus stops

--- app/src/main/java/com/dreadboy/marprom_voznired/MainActivity.kt
+++ app/src/main/java/com/dreadboy/marprom_voznired/MainActivity.kt
@@ -1,23 +1,38 @@
 package com.dreadboy.marprom_voznired
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringSetPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.dreadboy.marprom_voznired.ui.theme.MarpromVozniRedTheme
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore("vozni_red")
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val vm = TimetableViewModel();
+        val vm = TimetableViewModel(this)
         setContent {
             MarpromVozniRedTheme {
                 Layout {
-                    TimetableLoader(vm) { Timetable(it) }
+                    TimetableLoader(vm) {
+                        Timetable(
+                            it
+                        )
+                    }
                 }
             }
         }
@@ -25,30 +40,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TimetableLoader(vm: TimetableViewModel, content: @Composable (stops: List<Stop>) -> Unit) {
+fun TimetableLoader(
+    vm: TimetableViewModel, content: @Composable (
+        vm: TimetableViewModel
+    ) -> Unit
+) {
     LaunchedEffect(Unit) {
         vm.getTimetable();
     }
 
-    content(vm.stops)
+    content(vm)
 }
 
-class TimetableViewModel : ViewModel() {
+public class TimetableViewModel(context: Context) : ViewModel() {
+
     private val _stops = mutableStateListOf<Stop>()
-    var errorMessage: String by mutableStateOf("")
     val stops: List<Stop>
         get() = _stops
+
+    private val key = stringSetPreferencesKey("favouriteStops");
+    private val dataStore = context.dataStore;
+    val favouriteStops: Flow<List<String>> =
+        dataStore.data.map { it[key]?.toList() ?: listOf() }
 
     fun getTimetable() {
         viewModelScope.launch {
             val apiService = TimetableService.getInstance()
-            try {
-                _stops.clear()
-                _stops.addAll(apiService.getTimetable())
+            _stops.clear()
+            _stops.addAll(apiService.getTimetable())
+        }
+    }
 
-            } catch (e: Exception) {
-                errorMessage = e.message.toString()
-            }
+    fun saveFavourites(favourites: List<String>) {
+        viewModelScope.launch {
+            dataStore.edit { it[key] = favourites.toSet() }
         }
     }
 }
--- app/src/main/java/com/dreadboy/marprom_voznired/Timetable.kt
+++ app/src/main/java/com/dreadboy/marprom_voznired/Timetable.kt
@@ -3,37 +3,47 @@ package com.dreadboy.marprom_voznired
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ListItem
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun Timetable(stops: List<Stop>) {
-    val routes = stops
-        .flatMap { stop ->
-            stop.routes.map { route ->
-                object {
-                    var stop = stop
-                    val route = route
-                }
-            }
-        }
-        .groupBy { it.route.id }
-        .map {
-            object {
-                val route = it.value[0].route
-                val stops = it.value.map { obj -> obj.stop }
-            }
-        }
+fun Timetable(
+    vm: TimetableViewModel
+) {
+    val stops = vm.stops;
+    val favouriteStops by vm.favouriteStops.collectAsState(listOf());
 
     LazyColumn(modifier = Modifier.fillMaxHeight()) {
         items(stops) { stop ->
+            val isFavourite = favouriteStops.contains(stop.id)
             ListItem(
                 text = { Text(stop.name) },
-                secondaryText = { Text("Linije: " + stop.routes.joinToString(", ") { it.id })},
+                secondaryText = { Text("Linije: " + stop.routes.joinToString(", ") { it.id }) },
+                trailing = {
+                    IconButton(
+                        onClick = {
+                            val newFavourites =
+                                if (isFavourite) favouriteStops.filter { stop.id != it } else favouriteStops.plus(
+                                    stop.id
+                                )
+                            vm.saveFavourites(newFavourites)
+                        },
+                    ) {
+                        Icon(
+                            if (isFavourite) Icons.Outlined.Favorite else Icons.Outlined.FavoriteBorder,
+                            null,
+                            tint = MaterialTheme.colors.secondary
+                        )
+                    }
+                }
             )
         }
     }
