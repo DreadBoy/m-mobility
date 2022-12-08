b3906663ea383fd8f6735cfdd05031161a7a390f
Matic Leva <vertical3life@gmail.com>
2022-12-08 21:32:18 +0100

    Reuse colors from colors.xml

--- app/src/main/java/com/dreadboy/marprom_voznired/ui/theme/Theme.kt
+++ app/src/main/java/com/dreadboy/marprom_voznired/ui/theme/Theme.kt
@@ -5,7 +5,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import com.dreadboy.marprom_voznired.R
 
 @Composable
 fun MarpromVozniRedTheme(
@@ -15,15 +16,15 @@ fun MarpromVozniRedTheme(
 ) {
     MaterialTheme(
         colors = if (isSystemInDarkTheme()) darkColors(
-            primary = Color(0xFFA62934),
-            onPrimary = Color.White,
-            secondary = Color(0xFFD9AA1E),
-            onSecondary = Color.Black,
+            primary = colorResource(R.color.primary_dark),
+            onPrimary = colorResource(R.color.white),
+            secondary = colorResource(R.color.secondary_dark),
+            onSecondary = colorResource(R.color.black),
         ) else lightColors(
-            primary = Color(0xFFF23041),
-            onPrimary = Color.Black,
-            secondary = Color(0xFFF2CC0F),
-            onSecondary = Color.Black,
+            primary = colorResource(R.color.primary_light),
+            onPrimary = colorResource(R.color.black),
+            secondary = colorResource(R.color.secondary_light),
+            onSecondary = colorResource(R.color.black),
         ),
         content = content
     )
